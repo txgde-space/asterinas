b2a7c7cc66b1a1db36a7d4a5d016da88ab624267
Thou <tongxuguang23@163.com>
2026-06-04 19:42:03 +0800

    指标一：接入内核 IP raw socket 模块

--- kernel/src/net/socket/ip/mod.rs
+++ kernel/src/net/socket/ip/mod.rs
@@ -4,9 +4,12 @@ mod addr;
 mod common;
 mod datagram;
 pub mod options;
+mod raw;
+mod raw_observer;
 mod stream;
 
 pub use datagram::DatagramSocket;
 pub(in crate::net) use datagram::observer::DatagramObserver;
+pub use raw::RawSocket;
 pub(in crate::net) use stream::observer::StreamObserver;
 pub use stream::{StreamSocket, options as stream_options};
