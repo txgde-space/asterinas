e0747d8e7941230fec70d0029be9b291ad1a6e16
Thou <tongxuguang23@163.com>
2026-06-25 21:18:34 +0800

    指标一/二/三：接入网络兼容测试入口

--- test/initramfs/src/regression/network/run_test.sh
+++ test/initramfs/src/regression/network/run_test.sh
@@ -16,16 +16,30 @@ sleep 0.2
 sleep 0.2
 ./unix_client
 
+./linux_socket_compat_common
+./linux_socket_compat
+./icmp_raw_socket
+./netfilter_rules
+sh ./ping_loopback.sh
+./listen_autobind
 ./listen_backlog
+./inaddr_any
+./getsockname_any
+./localhost_loopback
+./tcp_accept_model
+./ipv6_any
 ./privileged_ports
 ./send_buf_full
+./socket_buffer_defaults
 ./sendmmsg
 ./socketpair
 ./sockoption
 ./sockoption_unix
 ./tcp_err
 ./tcp_poll
+./socket_readiness
 ./tcp_reuseaddr
+./tcp_wrapped_buffer_io
 ./udp_broadcast
 ./udp_err
 ./unix_datagram_err
